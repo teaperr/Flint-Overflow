bcff5f9347328c95bf07515c8ca11060d1011262
teaperr <143550106+teaperr@users.noreply.github.com>
2023-11-06 20:16:29 +0100

    init

---
+++ src/main/java/net/teaper/flintoverflow/FlintOverflow.java
@@ -0,0 +1,13 @@
+package net.teaper.flintoverflow;
+
+import net.fabricmc.api.ModInitializer;
+
+public class FlintOverflow implements ModInitializer {
+    /**
+     * Runs the mod initializer.
+     */
+    @Override
+    public void onInitialize() {
+
+    }
+}
